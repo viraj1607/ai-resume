faa3bc027f1dc197f4f3bcc06c5f2aea75a52943
Viraj Merai <merai.viraj@gmail.com>
2025-04-04 18:51:45 -0400

    set up form section and preview section

--- src/pages/dashboard/resume/[resumeId]/EditResume/EditResume.jsx
+++ src/pages/dashboard/resume/[resumeId]/EditResume/EditResume.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import FormSection from "@/components/custom/FormSection";
+import PreviewSection from "@/components/custom/PreviewSection";
+import React from "react";
 
 function EditResume() {
   return (
-    <div>EditResume</div>
-  )
+    <div className="gird grid-cols-1 md:grid-cols-2 p-10 gap-10">
+      <FormSection />
+      <PreviewSection />
+    </div>
+  );
 }
 
-export default EditResume
+export default EditResume;
